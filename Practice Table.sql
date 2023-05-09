d00619b28b579bc0784e2d347e16e0a2fa362c32
Conrad-Esconde <98925409+Conrad-Esconde@users.noreply.github.com>
2023-05-09 12:03:31 +0800

    Add files via upload

---
+++ Practice Table.sql
@@ -0,0 +1,3 @@
+select * from customer.abc_computer;
+select SerialNumber, Make from customer.abc_computer where Graphics = 'Integrated Intel HD Graphics 5000' Or DiskSize = '1.0 Tbytes';
+select * from customer.abc_computer where SerialNumber = '6541001' or Model = 'Optiflex 9021';
